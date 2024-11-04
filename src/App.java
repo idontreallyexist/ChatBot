69ab588b2def35b788f132006bfea3de1450b628
idontreallyexist <kevinalexyu09@gmail.com>
2024-11-04 23:01:00 +0000

    added loop and a clear screen method

--- src/App.java
+++ src/App.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
+        clearScreen();
         Scanner input = new Scanner (System.in);
         System.out.println("MiniWag: Hello and welcome to Sandwich Emperor! What do you want today?");
-        action = input.nextLine();
+        while(true){
+            String action = input.nextLine();
+            clearScreen();
+        }
     }
+    public static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }  
 }
